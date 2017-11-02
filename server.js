e871e3e10eed71038bd624d5354e87bfa5add2d4
Josselin Buils <josselin.buils@gmail.com>
2017-11-02 11:54:03 +0100

    Add X-Frame-Options header

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('X-Frame-Options', 'ALLOW-FROM http://localhost:4200');
     next();
 });
 
